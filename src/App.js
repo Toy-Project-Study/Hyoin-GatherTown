371524c42b84c8902f4d1cd60ebf59797f5e01ef
Hyoin-kim <hi980506@naver.com>
2021-12-02 23:40:09 +0900

    chore : Route 설정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import Header from "./components/Header";
 import Main from "./pages/Main";
+import Start from "./pages/Start";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
 import GlobalStyle from "./styles/global-style";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Header />
-      <Main />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Routes>
+          <Route exact path="/" element={<Start />} />
+          <Route path="/main" element={<Main />} />
+          <Route path="/*" element={<p>Page Not Found</p>} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
