a8a37d675abfbe8140704611cc466f1e9867eacc
Hyoin-Kim <hi980506@naver.com>
2021-12-06 19:22:32 +0900

    chore : 이미지 export 정리

---
+++ src/assets/index.js
@@ -0,0 +1,26 @@
+export const userImages = {
+  ninja: {
+    stand: require("./images/totoro01.png"),
+  },
+  mario: {
+    stand: require("./images/totoro08.png"),
+  },
+  orange: {
+    stand: require("./images/totoro05.png"),
+  },
+  jiwoo: {
+    stand: require("./images/totoro04.png"),
+  },
+  pig: {
+    stand: require("./images/totoro02.png"),
+  },
+  superman: {
+    stand: require("./images/totoro03.png"),
+  },
+  gentleman: {
+    stand: require("./images/totoro07.png"),
+  },
+  foreigner: {
+    stand: require("./images/totoro06.png"),
+  },
+};
